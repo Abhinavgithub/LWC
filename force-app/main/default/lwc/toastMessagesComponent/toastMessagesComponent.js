886918042a1578cf9d16919a35229e9f8953c81f
Abhinav <adikari.abhinav@gmail.com>
2023-11-30 22:59:32 +0530

    Toast Notifications

--- force-app/main/default/lwc/toastMessagesComponent/toastMessagesComponent.js
+++ force-app/main/default/lwc/toastMessagesComponent/toastMessagesComponent.js
@@ -1,9 +1,16 @@
 import { LightningElement } from "lwc";
 import { ShowToastEvent } from "lightning/platformShowToastEvent";
+import ToastContainer from "lightning/toastContainer";
 export default class ToastMessagesComponent extends LightningElement {
   title;
   message;
   variant;
+
+  connectedCallback() {
+    const toastContainer = ToastContainer.instance();
+    toastContainer.maxShown = 5;
+    toastContainer.toastPosition = "top-right";
+  }
 
   get options() {
     return [
@@ -29,5 +36,20 @@ export default class ToastMessagesComponent extends LightningElement {
       variant: this.variant
     });
     this.dispatchEvent(evt);
+  }
+  showLinkedNotification() {
+    const linkedevt = new ShowToastEvent({
+      title: this.title,
+      variant: this.variant,
+      message: "See it {0}!",
+      messageData: [
+        "Salesforce",
+        {
+          url: "http://www.salesforce.com/",
+          label: "here"
+        }
+      ]
+    });
+    this.dispatchEvent(linkedevt);
   }
 }
