296b9256435e4ebb3cd0767764028fb937286507
Abhinav Singh <adikari.abhinav@gmail.com>
2024-06-13 01:54:34 +0530

    added Year dropdown

--- force-app/main/default/lwc/searchTMDBMovies/searchTMDBMovies.js
+++ force-app/main/default/lwc/searchTMDBMovies/searchTMDBMovies.js
@@ -1,3 +1,18 @@
 import { LightningElement } from 'lwc';
 
-export default class SearchTMDBMovies extends LightningElement {}
+export default class SearchTMDBMovies extends LightningElement {
+  options;
+  value;
+  connectedCallback() {
+    const currentYear = new Date().getFullYear();
+    const yearRange = 100; // Adjust the range as needed
+    const years = [];
+    for (let i = currentYear - yearRange; i <= currentYear; i++) {
+      years.push({ label: `${i}`, value: i });
+    }
+    this.options = years;
+  }
+  handleYearSelection(event) {
+    this.value = event.detail.value;
+  }
+}
